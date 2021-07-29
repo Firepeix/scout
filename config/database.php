e34576d375d7b4688eca62eb94be7c74af5d9623
Arthur Fernandes <arthur@onfly.com.br>
2021-07-29 11:22:07 -0300

    Unificando aplicacoes

--- config/database.php
+++ config/database.php
@@ -106,10 +106,10 @@ return [
         ],
         'chest' => [
             'driver' => 'mongodb',
-            'host' => env('DB_CHEST_HOST', '127.0.0.1'),
-            'port' => env('DB_CHEST_PORT', 27017),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 27017),
             'database' => env('DB_CHEST_DATABASE', 'homestead'),
-            'dsn' => env('DB_CHEST_DSN', 'homestead'),
+            'dsn' => env('DB_DSN', 'homestead'),
             'options' => [
                 // here you can pass more settings to the Mongo Driver Manager
                 // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
