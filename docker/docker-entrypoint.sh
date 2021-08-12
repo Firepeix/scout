eb1feb3a0e1a1ee879166cdf8de55d77b8143b1d
Arthur Fernandes <arthurhmf@gmail.com>
2021-08-12 00:09:08 -0300

    Corrigindo entrypoint
    Signed-off-by: Arthur Fernandes <arthurhmf@gmail.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-
 if [ ! -d "/application/vendor" ]; then
   cd /application && composer install
 fi
@@ -16,7 +15,7 @@ if [ $APP_ENV != 'local' ]; then
 
   grep -q '^newrelic.distributed_tracing_enabled' $NEW_RELIC_INI &&
     sed -i 's/^newrelic.distributed_tracing_enabled.*/newrelic.distributed_tracing_enabled=true/' $NEW_RELIC_INI ||
-    echo 'newrelic.distributed_tracing_enabled=true' >>$NEW_RELIC_INI
+    echo 'newrelic.distributed_tracing_enabled=true' >> $NEW_RELIC_INI
   cd /application && php artisan migrate --seed && php artisan log:clean
   if [ ! -d "/application/app/Infrastructure/Supervisor/services/scout-worker-conf" ]; then
     cp /application/app/Infrastructure/Supervisor/scout-worker-conf /application/app/Infrastructure/Supervisor/services/scout-worker-conf
