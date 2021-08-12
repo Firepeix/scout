a17cd0b4f5d4e6efc44286bb267fce31c752a4ba
Arthur Fernandes <arthurhmf@gmail.com>
2021-08-12 00:01:36 -0300

    Criando Prod
    Signed-off-by: Arthur Fernandes <arthur@onfly.com.br>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env sh
-NEW_RELIC_INI=$(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/20-newrelic.ini
-NEW_RELIC_KEY=$(grep '^NEW_RELIC_KEY=' /application/.env | sed -e 's/NEW_RELIC_KEY=//')
 
-if [ ! -d "/application/vendor" ]
-then
-    cd /application && composer install
+if [ ! -d "/application/vendor" ]; then
+  cd /application && composer install
 fi
 
-echo "starting" > /application/storage/logs/app.log
-echo "starting" > /application/storage/logs/backup.log
+echo "starting" >/application/storage/logs/app.log
+echo "starting" >/application/storage/logs/backup.log
 
-
-#INSTALL NEW RELIC
-
-sed -i \
+if [ $APP_ENV != 'local' ]; then
+  sed -i \
     -e "s/newrelic.license =.*/newrelic.license=\"${NEW_RELIC_KEY}\"/" \
     -e "s/newrelic.appname[[:space:]]=[[:space:]].*/newrelic.appname=\"Scout\"/" \
     -e "s/;\?newrelic.framework =.*/newrelic.framework = \llaravel/" \
     $NEW_RELIC_INI
 
-grep -q '^newrelic.distributed_tracing_enabled' $NEW_RELIC_INI && \
- sed -i 's/^newrelic.distributed_tracing_enabled.*/newrelic.distributed_tracing_enabled=true/' $NEW_RELIC_INI || \
- echo 'newrelic.distributed_tracing_enabled=true' >> $NEW_RELIC_INI
-
-cd /application && php artisan migrate --seed && php artisan log:clean
-exec supervisord -c /application/app/Infrastructure/Supervisor/supervisord.conf
+  grep -q '^newrelic.distributed_tracing_enabled' $NEW_RELIC_INI &&
+    sed -i 's/^newrelic.distributed_tracing_enabled.*/newrelic.distributed_tracing_enabled=true/' $NEW_RELIC_INI ||
+    echo 'newrelic.distributed_tracing_enabled=true' >>$NEW_RELIC_INI
+  cd /application && php artisan migrate --seed && php artisan log:clean
+  if [ ! -d "/application/app/Infrastructure/Supervisor/services/scout-worker-conf" ]; then
+    cp /application/app/Infrastructure/Supervisor/scout-worker-conf /application/app/Infrastructure/Supervisor/services/scout-worker-conf
+  fi
+  exec supervisord -c /application/app/Infrastructure/Supervisor/supervisord.conf
+else
+  exec supervisord -c /application/app/Infrastructure/Supervisor/supervisord.conf
+fi
