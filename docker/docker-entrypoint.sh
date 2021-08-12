63d0b5ff025f6fd3e0f9e02c4dd0c9754c625a67
Arthur Fernandes <arthurhmf@gmail.com>
2021-08-12 00:45:08 -0300

    Ajustando confs
    Signed-off-by: Arthur Fernandes <arthurhmf@gmail.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -18,9 +18,9 @@ if [ $APP_ENV != 'local' ]; then
   grep -q '^newrelic.distributed_tracing_enabled' $NEW_RELIC_INI &&
     sed -i 's/^newrelic.distributed_tracing_enabled.*/newrelic.distributed_tracing_enabled=true/' $NEW_RELIC_INI ||
     echo 'newrelic.distributed_tracing_enabled=true' >> $NEW_RELIC_INI
-  cd /application && php artisan migrate --seed && php artisan log:clean
-  if [ ! -d "/application/app/Infrastructure/Supervisor/services/scout-worker-conf" ]; then
-    cp /application/app/Infrastructure/Supervisor/scout-worker-conf /application/app/Infrastructure/Supervisor/services/scout-worker-conf
+  cd /application && php artisan migrate --seed --force && php artisan log:clean
+  if [ ! -d "/application/app/Infrastructure/Supervisor/services/scout-worker.conf" ]; then
+    cp /application/app/Infrastructure/Supervisor/scout-worker.conf /application/app/Infrastructure/Supervisor/services/scout-worker.conf
   fi
   exec supervisord -c /application/app/Infrastructure/Supervisor/supervisord.conf
 else
