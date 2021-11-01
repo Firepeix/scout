e7f439eb9d5eb20033b33d44c8319c096bc72d4f
Arthur Fernandes <arthurhmf@gmail.com>
2021-11-01 13:12:50 -0300

    feat: adicionando rotas por attributos
    Signed-off-by: Arthur Fernandes <arthurhmf@gmail.com>

---
+++ src/Shared/Infrastructure/Providers/ModuleServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Shared\Infrastructure\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    public function register() : void
+    {
+        //$this->app->register(RouteServiceProvider::class);
+    }
+}
